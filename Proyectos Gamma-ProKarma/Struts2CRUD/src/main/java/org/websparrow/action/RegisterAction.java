428af5ad7f06614b93239c31ab1bd049716b3ac6
jclopezmolinar <50610603+jclopezmolinar@users.noreply.github.com>
2019-09-12 15:09:38 -0600

    Added Struts2CRUD

---
+++ Proyectos Gamma-ProKarma/Struts2CRUD/src/main/java/org/websparrow/action/RegisterAction.java
@@ -0,0 +1,81 @@
+package org.websparrow.action;
+
+import org.websparrow.dao.Admin;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport{
+
+	private static final long serialVersionUID = 4520047631640187733L;
+	private String uname;
+	private String uemail;
+	private String upass;
+	private String udeg;
+	private String msg = "";
+	Admin admin = null;
+	int ctr = 0;
+	
+	@Override
+	public String execute() throws Exception{
+		admin = new Admin();
+		try{
+			ctr = admin.registerUser(uname, uemail, upass, udeg);
+			if(ctr > 0){
+				msg = "Registration Successful!";
+			} else{
+				msg = "Some error";
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return "REGISTER";
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUemail() {
+		return uemail;
+	}
+
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+
+	public String getUpass() {
+		return upass;
+	}
+
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+
+	public String getUdeg() {
+		return udeg;
+	}
+
+	public void setUdeg(String udeg) {
+		this.udeg = udeg;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public int getCtr() {
+		return ctr;
+	}
+
+	public void setCtr(int ctr) {
+		this.ctr = ctr;
+	}
+}
